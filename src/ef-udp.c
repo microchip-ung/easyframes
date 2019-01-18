3a60c144799021895808672de8fcedc493cae375
Joergen Egebaek Andreasen <joergen.andreasen@microsemi.com>
2019-01-18 11:57:12 +0100

    Added tcp.

--- src/ef-udp.c
+++ src/ef-udp.c
@@ -31,7 +31,7 @@ hdr_t HDR_IPV6_PSEUDO = {
             sizeof(IPv6_PSEUDO_FIELDS[0]),
 };
 
-static int udp_fill_defaults(struct frame *f, int stack_idx) {
+static int udp_tcp_fill_defaults(struct frame *f, int stack_idx) {
     int i, udp_len = 0;
     char buf[16];
     hdr_t *h = f->stack[stack_idx];
@@ -42,7 +42,7 @@ static int udp_fill_defaults(struct frame *f, int stack_idx) {
         udp_len += f->stack[i]->size;
     }
 
-    if (!len->val) {
+    if (len && !len->val) { // This field is only present in UDP
         snprintf(buf, 16, "%d", udp_len);
         buf[15] = 0;
         len->val = parse_bytes(buf, 2);
@@ -67,8 +67,10 @@ static int udp_fill_defaults(struct frame *f, int stack_idx) {
         find_field(pseudo_hdr, "sip")->val = bclone(find_field(ll, "sip")->val);
         find_field(pseudo_hdr, "dip")->val = bclone(find_field(ll, "dip")->val);
 
-        // Set proto to UDP in pseudo header
-        find_field(pseudo_hdr, "proto")->val = parse_bytes("17", 1);
+        // Set proto in pseudo header
+        snprintf(buf, 16, "%d", h->type);
+        buf[15] = 0;
+        find_field(pseudo_hdr, "proto")->val = parse_bytes(buf, 1);
 
         // Set len in pseudo header. Size of len is different in ipv4 and 6
         snprintf(buf, 16, "%d", udp_len);
@@ -118,22 +120,87 @@ static hdr_t HDR_UDP = {
     .type = 17,
     .fields = UDP_FIELDS,
     .fields_size = sizeof(UDP_FIELDS) / sizeof(UDP_FIELDS[0]),
-    .frame_fill_defaults = udp_fill_defaults,
+    .frame_fill_defaults = udp_tcp_fill_defaults,
+    .parser = hdr_parse_fields,
+};
+
+static field_t TCP_FIELDS[] = {
+    { .name = "sport",
+      .help = "Source Port Number, e.g. 22 for SSH",
+      .bit_width =  16 },
+    { .name = "dport",
+      .help = "Destination Port Number, e.g. 22 for SSH",
+      .bit_width =  16 },
+    { .name = "seqn",
+      .help = "Sequence number",
+      .bit_width =  32 },
+    { .name = "ackn",
+      .help = "Acknowledgement number",
+      .bit_width =  32 },
+    { .name = "doff",
+      .help = "Data offset, size of TCP header in 32-bit words",
+      .bit_width =   4 },
+    { .name = "resv",
+      .help = "Reserved, must be zero",
+      .bit_width =   6 },
+    { .name = "urg",
+      .help = "Urgent Pointer field significant",
+      .bit_width =   1 },
+    { .name = "ack",
+      .help = "Acknowledgment field significant",
+      .bit_width =   1 },
+    { .name = "psh",
+      .help = "Push Function",
+      .bit_width =   1 },
+    { .name = "rst",
+      .help = "Reset the connection",
+      .bit_width =   1 },
+    { .name = "syn",
+      .help = "Synchronize sequence numbers",
+      .bit_width =   1 },
+    { .name = "fin",
+      .help = "No more data from sender",
+      .bit_width =   1 },
+    { .name = "win",
+      .help = "Window",
+      .bit_width =  16 },
+    { .name = "chksum",
+      .help = "Checksum",
+      .bit_width =  16 },
+    { .name = "urgp",
+      .help = "Urgent Pointer",
+      .bit_width =  16 },
+};
+
+static hdr_t HDR_TCP = {
+    .name = "tcp",
+    .help = "Transmission Control Protocol",
+    .type = 6,
+    .fields = TCP_FIELDS,
+    .fields_size = sizeof(TCP_FIELDS) / sizeof(TCP_FIELDS[0]),
+    .frame_fill_defaults = udp_tcp_fill_defaults,
     .parser = hdr_parse_fields,
 };
 
 void udp_init() {
-    def_offset(&HDR_UDP);
     def_offset(&HDR_IPV4_PSEUDO);
     def_offset(&HDR_IPV6_PSEUDO);
 
+    def_offset(&HDR_UDP);
+    def_offset(&HDR_TCP);
+    def_val(&HDR_TCP, "doff", "5");
+
     hdr_tmpls[HDR_TMPL_UDP] = &HDR_UDP;
+    hdr_tmpls[HDR_TMPL_TCP] = &HDR_TCP;
 }
 
 void udp_uninit() {
-    uninit_frame_data(&HDR_UDP);
     uninit_frame_data(&HDR_IPV4_PSEUDO);
     uninit_frame_data(&HDR_IPV6_PSEUDO);
 
+    uninit_frame_data(&HDR_UDP);
+    uninit_frame_data(&HDR_TCP);
+
     hdr_tmpls[HDR_TMPL_UDP] = 0;
+    hdr_tmpls[HDR_TMPL_TCP] = 0;
 }
