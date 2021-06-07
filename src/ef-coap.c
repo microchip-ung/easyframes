7d7cd50c987d0290d45701c5075cae1b7b43bc30
Robert Wilhelm <robert.wilhelm@microchip.com>
2021-06-07 17:51:41 +0200

    Supports options with 1 num/val pair.

--- src/ef-coap.c
+++ src/ef-coap.c
@@ -9,8 +9,6 @@ enum {
     COAP_FIELD_CODE,
     COAP_FIELD_MESSAGE_ID,
     COAP_FIELD_TOKEN,
-//    COAP_FIELD_OPTIONS,
-//    COAP_FIELD_PARMS,
 
     COAP_FIELD_LAST
 };
@@ -22,15 +20,12 @@ enum {
     COAP_OPT_FIELD_LAST
 };
 
-<<<<<<< HEAD
 /* neede to calculate the option delta */
 static uint8_t opt_num_last;
 static buf_t *opt_num;
 static buf_t *opt_val;
 
 
-=======
->>>>>>> 5ee350bb2b9a4e49f0db401b395f03fffa0b98f5
 static buf_t *coap_parse_code(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
     buf_t *b = balloc(1);
     uint8_t tmp;
@@ -88,45 +83,19 @@ buf_t *coap_parse_token(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
     return b;
 }
 
-buf_t *coap_parse_options(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
-//    int i, offset = 0;
-    buf_t *b;
-
-    b = parse_var_bytes_hex(s,1);
-
-    if (!b) {
-        return 0;
-    }
-
-    //hdr->fields[COAP_FIELD_OPTIONS].bit_width = b->size * 8;
-    hdr->fields[0].bit_width = b->size * 8;
-
-/*
-    for (i = 0; i < COAP_FIELD_LAST; ++i) {
-        hdr->fields[i].bit_offset = offset;
-        offset = hdr->fields[i].bit_offset + hdr->fields[i].bit_width;
-    }
-
-    hdr->size = offset / 8;
-*/
-
-    return b;
-}
-
 buf_t *coap_parse_opt_num(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
-    opt_num = parse_bytes(s, 1);
+    opt_num = parse_bytes(s, 2);
 
     return 0;
 }
 
 buf_t *coap_parse_opt_val(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
-    //opt_val = parse_bytes(s, 1);
-    parse_var_bytes(&opt_val, 2, &s);
+    opt_val = parse_var_bytes_hex(s, 1);
+
     return 0;
 }
 
 buf_t *coap_parse_parms(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
-//    int i, offset = 0;
     buf_t *bb, *b;
 
     bb = parse_var_bytes_hex(s, 1);
@@ -143,15 +112,7 @@ buf_t *coap_parse_parms(hdr_t *hdr, int hdr_offset, const char *s, int bytes) {
     *(b->data) = 0xFF;
 
 
-//    hdr->fields[COAP_FIELD_PARMS].bit_width = b->size * 8;
     hdr->fields[0].bit_width = b->size * 8;
-
-/*    for (i = 0; i < COAP_FIELD_LAST; ++i) {
-        hdr->fields[i].bit_offset = offset;
-        offset = hdr->fields[i].bit_offset + hdr->fields[i].bit_width;
-    }
-    hdr->size = offset / 8;
-*/
 
     bfree(bb);
     return b;
@@ -163,10 +124,6 @@ static int coap_fill_defaults(struct frame *f, int stack_idx) {
     field_t *tkl = find_field(h, "tkl");
 
     if (!tkl->val) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 5ee350bb2b9a4e49f0db401b395f03fffa0b98f5
         snprintf(buf, 16, "%d", BIT_TO_BYTE(h->fields[COAP_FIELD_TOKEN].bit_width));
 
         tkl->val = parse_bytes(buf, 1);
@@ -202,17 +159,7 @@ static field_t COAP_FIELDS[] = {
     { .name = "token",
       .help = "Token length must not greater than 8 bytes. (optional)",
       .bit_width = 0,
-      .parser = coap_parse_token }/*,  
-    [COAP_FIELD_OPTIONS]
-    { .name = "opt",
-      .help = "Options, provided as hex string.  (optional)",
-      .bit_width = 0,
-      .parser = coap_parse_options },  
-    [COAP_FIELD_PARMS]
-    { .name = "par",
-      .help = "Parameters. (optional)",
-      .bit_width = 0,
-      .parser = coap_parse_parms }  */
+      .parser = coap_parse_token }
 };
 
 static hdr_t HDR_COAP = {
@@ -225,21 +172,92 @@ static hdr_t HDR_COAP = {
 };
 
 static int options_fill_defaults(struct frame *f, int stack_idx) {
-<<<<<<< HEAD
-    uint8_t delta;
+    buf_t *bb, *b;
+    
+    uint16_t delta;
+    uint16_t delta_ext = 0;
+    uint16_t len = 0;
+    uint16_t len_ext = 0;
+    uint16_t i;
+    uint16_t temp;
+    int offset = 0;
+
+
 
     if (!opt_num) {
         return 0;
     }
 
-    delta = opt_num->data[0] - opt_num_last;
-    (void)delta;
+    if (opt_val){
+        len = opt_val->size; 
+    }
+
+    temp = opt_num->data[0] * 256 + opt_num->data[1];
+    if (temp < opt_num_last) {
+        return 0;
+    }
+
+    b = balloc(5);
+    
+    /* calculate delta and delta-ext values */
+    delta = temp - opt_num_last;
+    opt_num_last = temp;
+
+    i = 1;
+    if (delta > 12) {
+        if (delta > 268) {
+            delta_ext        = delta - 269;
+            delta            = 14;
+            *(b->data + i++) = delta_ext >> 8;
+            *(b->data + i++) = delta_ext & 0xFF;
+        }
+        else {
+            delta_ext        = delta - 13;
+            delta            = 13;
+            *(b->data + i++) = delta_ext;
+        }
+    }    
+    *(b->data) = delta << 4;
 
 
+    if (len > 12) {
+        if (len > 268) {
+            len_ext          = len - 269;
+            len              = 14;
+            *(b->data + i++) = len_ext >> 8;
+            *(b->data + i++) = len_ext & 0xFF;
+        }
+        else {
+            len_ext          = len - 13;
+            len              = 13;
+            *(b->data + i++) = len_ext;
+        }
+    } 
 
+    *(b->data) |= len & 0x0F;
 
-=======
->>>>>>> 5ee350bb2b9a4e49f0db401b395f03fffa0b98f5
+    bb = balloc(i + opt_val->size);
+    if (opt_val->size != 0) {
+        memcpy(bb->data,b->data, i);
+        memcpy(bb->data + i, opt_val->data, opt_val->size);
+    }
+
+    hdr_t   *hdr = f->stack[stack_idx];
+    field_t *num = find_field(hdr, "num");
+
+    if (num) {
+        num->val = bb;
+    }
+
+    hdr->fields[COAP_OPT_FIELD_NUM].bit_width = num->val->size * 8;
+
+    for (i = 0; i < COAP_OPT_FIELD_LAST; ++i) {
+        hdr->fields[i].bit_offset = offset;
+        offset = hdr->fields[i].bit_offset + hdr->fields[i].bit_width;
+    }
+
+    hdr->size = offset / 8;
+
     return 0;
 }
 
@@ -272,22 +290,14 @@ static int parameters_fill_defaults(struct frame *f, int stack_idx) {
 static field_t COAP_PARMS_FIELDS[] = {  
 //    [COAP_FIELD_PARMS]
     { .name = "par",
-<<<<<<< HEAD
       .help = "CoAP Parameter field.",
-=======
-      .help = "CoAP Parameter field. (optional)",
->>>>>>> 5ee350bb2b9a4e49f0db401b395f03fffa0b98f5
       .bit_width = 0,
       .parser = coap_parse_parms }
 };
 
 static hdr_t HDR_COAP_PARMS = {
     .name = "coap-parms",
-<<<<<<< HEAD
-    .help = "Constrained Application Protocol Parameters. (o)ptional)",
-=======
-    .help = "Constrained Application Protocol Parameters",
->>>>>>> 5ee350bb2b9a4e49f0db401b395f03fffa0b98f5
+    .help = "Constrained Application Protocol Parameters. (optional)",
     .fields = COAP_PARMS_FIELDS,
     .fields_size = sizeof(COAP_PARMS_FIELDS) / sizeof(COAP_PARMS_FIELDS[0]),
     .frame_fill_defaults = parameters_fill_defaults,
@@ -309,11 +319,8 @@ void coap_init() {
     hdr_tmpls[HDR_TMPL_COAP]         = &HDR_COAP;
     hdr_tmpls[HDR_TMPL_COAP_OPTIONS] = &HDR_COAP_OPTIONS;
     hdr_tmpls[HDR_TMPL_COAP_PARMS]   = &HDR_COAP_PARMS;
-<<<<<<< HEAD
 
     opt_num_last = 0;
-=======
->>>>>>> 5ee350bb2b9a4e49f0db401b395f03fffa0b98f5
 }
 
 void coap_uninit() {
